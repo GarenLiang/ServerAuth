1628ded615d11cc3eff2adb7dfeb14c5a0148fa7
GarenLiang <ideabreeze@gmail.com>
2017-03-20 18:28:58 -0700

    User Create

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,23 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true' });
+  const email = req.body.email;
+  const password = req.body.password;
+  //See user exist or not
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
+    // if exist, return an error
+    if(existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+    //if not exist,create a new
+    const user = new User({
+      email: email,
+      password: password
+    });
+    user.save(function(err) {
+      if (err) { return next(err); }
+      res.json({ success: true });
+    });
+  });
 }
