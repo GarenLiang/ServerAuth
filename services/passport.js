7857080abaf330f61b460cffbd8d208635c80225
GarenLiang <ideabreeze@gmail.com>
2017-03-21 22:14:56 -0700

    Signin

--- services/passport.js
+++ services/passport.js
@@ -14,7 +14,14 @@ const localLogin = new LocalStrategy({ usernameField: 'email' }, function(email,
 User.findOne({ email: email }, function(err, user) {
   if(err) { return done(err); }
   if(!user) { return done(null, false); }
-})
+
+  user.comparePassword(password, function(err, isMatch) {
+    if (err) { return done(err); }
+    if(!isMatch) { return done(null, false); }
+
+    return done(null, user);
+  });
+});
 });
 //setup options for jwt strategy
 const jwtOptions = {
@@ -35,3 +42,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 });
 //tell passport to use the strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
